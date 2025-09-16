cf4b3aafaefda52c7918fbc1e47fa2ce5a21c7c5
njinu0479 <njinu0479@gmail.com>
2025-09-16 21:11:34 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int num1, num2, result;
+
+    std::cin >> num1;
+    std::cin >> num2;
+
+    result = num1 - num2;
+    std::cout << result;
+
+    return 0;
+}
