0cd5b92ec8291469713b1535e16695d6b584dc62
njinu0479 <njinu0479@naver.com>
2025-10-27 01:13:17 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.cc
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -1,13 +1,15 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    int year;
+    int y;
 
-    std::cin >> year;
+    cin >> y;
 
-    if (year >= 4 && year % 400 == 0) std::cout << 1;
-    else if (year >= 4 && year % 4 == 0 && year % 100 != 0) std::cout << 1;
-    else std::cout << 0;
+    if (y >= 4 && y % 400 == 0) cout << 1;
+    else if (y >= 4 && y % 4 == 0 && y % 100 != 0) cout << 1;
+    else cout << 0;
 
     return 0;
 }
