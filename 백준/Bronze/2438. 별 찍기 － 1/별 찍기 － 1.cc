0a733f8523aa426c243073023a5e8c97798a54c5
남진우 <njinu0479@gmail.com>
2025-11-02 05:14:14 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -7,7 +7,7 @@ int main() {
     cin >> n;
     
     for(int i = 0; i < n; i++) {
-        for(int j = n - i; j <= n; j++) cout << "*";
+        for(int j = 0; j <= i; j++) cout << "*";
         cout << "\n";
     }
     
