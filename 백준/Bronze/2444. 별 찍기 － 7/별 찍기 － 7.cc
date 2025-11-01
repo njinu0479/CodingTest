20c63981a09ae3c629d6fd924dce38df6fb1e19a
njinu0479 <njinu0479@naver.com>
2025-11-02 07:11:40 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i <= n - 2; i++) {
+        for (int j = n - i - 1; j > 0; j--) {
+            cout << " ";
+        }
+        for (int j = 2 * i + 1; j > 0; j--) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+
+    for (int i = n - 1; i >= 0; i--) {
+        for (int j = 0; j < n - 1 - i; j++) {
+            cout << " ";
+        }
+        for (int j = 0; j < 2 * i + 1; j++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+}
