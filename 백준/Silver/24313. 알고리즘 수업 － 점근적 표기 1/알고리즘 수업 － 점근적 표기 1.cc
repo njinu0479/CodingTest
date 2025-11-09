8f61e630e05cf66b33c1a8d315bc45ddde0e1bf3
njinu0479 <njinu0479@naver.com>
2025-11-09 18:31:40 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int a0, a1, c, n0;
+    cin >> a1 >> a0 >> c >> n0;
+
+    if((a1 - c) * n0 + a0 <= 0 && a1 <= c) cout << 1;
+    else cout << 0;
+
+    return 0;
+}
