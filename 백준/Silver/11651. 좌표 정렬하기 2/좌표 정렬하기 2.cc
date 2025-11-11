2f747ae9d983787838da0d68e45c134badfd00b6
njinu0479 <njinu0479@naver.com>
2025-11-11 23:48:04 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 40 ms, Memory: 2808 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int compare(const pair<int, int>& a, const pair<int, int>& b) {
+    if (a.second != b.second) {
+        return a.second < b.second;
+    }
+    
+    return a.first < b.first;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    vector<pair<int, int>> v(n);
+    for(int i = 0; i < n; i++) cin >> v[i].first >> v[i].second;
+
+    sort(v.begin(), v.end(), compare);
+    for(int i = 0; i < n; i++) cout << v[i].first << ' ' << v[i].second << '\n';
+
+    return 0;
+}
