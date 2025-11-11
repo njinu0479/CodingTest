1bc7b858d8ad39bdffe944f13e9b95554e2248c5
njinu0479 <njinu0479@naver.com>
2025-11-12 07:34:26 +0900

    [Bronze V] Title: Shares, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3733. Shares/Shares.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, s;
+    while(cin >> n >> s) {
+        cout << s / (n + 1) << '\n';
+    }
+
+    return 0;
+}
