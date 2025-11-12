06fd9b819d56ea9119ac24b67f0487162a408c17
njinu0479 <njinu0479@naver.com>
2025-11-13 02:45:19 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 2.62 ms, Memory: 4.11 MB -BaekjoonHub

---
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str;
+    cin >> str;
+    
+    for(int i = 0; i < str.length(); i++) {
+        if(str[i] <= 'Z' && str[i] >= 'A') cout << (char)(str[i] + 32);
+        else cout << (char)(str[i] - 32);
+    } 
+    
+    return 0;
+}
