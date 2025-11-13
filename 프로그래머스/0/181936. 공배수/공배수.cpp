ec4d52bf29e0587e5bece2d55b5fd84ff6da3263
njinu0479 <njinu0479@naver.com>
2025-11-14 02:08:21 +0900

    [level 0] Title: 공배수, Time: 0.01 ms, Memory: 3.71 MB -BaekjoonHub

---
+++ 프로그래머스/0/181936. 공배수/공배수.cpp
@@ -0,0 +1,11 @@
+using namespace std;
+
+int solution(int number, int n, int m) {
+    int answer = 0;
+    
+    if(number % n == 0 && number % m == 0) {
+        answer = 1;
+    }
+    
+    return answer;
+}
