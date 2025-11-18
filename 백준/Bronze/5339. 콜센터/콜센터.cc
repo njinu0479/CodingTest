6a63616c68e48565ea993e6e7ec59d38e8de4b77
남진우 <njinu0479@naver.com>
2025-11-19 06:53:56 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cout << "     /~\\\n";
+    cout << "    ( oo|\n";
+    cout << "    _\\=/_\n";
+    cout << "   /  _  \\\n";
+    cout << "  //|/.\\|\\\\\n";
+    cout << " ||  \\ /  ||\n";
+    cout << "============\n";
+    cout << "|          |\n";
+    cout << "|          |\n";
+    cout << "|          |";
+
+    return 0;
+}
