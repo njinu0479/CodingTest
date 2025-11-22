5712fa800dfa54b0df398b086595654c9dc820ee
남진우 <njinu0479@naver.com>
2025-11-22 09:43:41 +0900

    [Silver III] Title: N과 M (1), Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int nums[9];
+bool visited[9];
+
+void dfs(int cnt) {
+    if(cnt == m) {
+        for(int i = 0; i < m; i++) cout << nums[i] << ' ';
+        cout << '\n';
+        return;
+    }
+
+    for(int i = 1; i <= n; i++) {
+        if(!visited[i]) {
+            visited[i] = true;
+            nums[cnt] = i;
+            dfs(cnt + 1);
+            visited[i] = false;
+        } 
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n >> m;
+    dfs(0);
+
+    return 0;
+}
