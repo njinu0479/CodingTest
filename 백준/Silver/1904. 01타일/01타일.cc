078278f5943384d5ff802a8463f021c0a51179d0
남진우 <njinu0479@naver.com>
2025-11-25 11:15:06 +0900

    [Silver III] Title: 01타일, Time: 8 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.cc
+++ 백준/Silver/1904. 01타일/01타일.cc
@@ -4,13 +4,6 @@ using namespace std;
 
 long long arr[1000001] = {0 };
 
-int seq(int n) {
-    if(n == 1) return 1;
-    if(n == 2) return 2;
-    if(arr[n] != 0) return arr[n];
-    return arr[n] = (seq(n - 1) + seq(n - 2)) % 15746;
-}
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -18,7 +11,12 @@ int main() {
     int n;
     cin >> n;
 
-    cout << seq(n);
+    arr[1] = 1, arr[2] = 2;
+    for(int i = 3; i <= n; i++) {
+        arr[i] = (arr[i - 1] + arr[i - 2]) % 15746;
+    }
+
+    cout << arr[n];
 
     return 0;
 }
