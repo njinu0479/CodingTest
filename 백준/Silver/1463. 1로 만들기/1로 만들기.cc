a08726e67664d096997985399bb14a0339699dd1
남진우 <njinu0479@naver.com>
2025-12-02 20:11:59 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    vector<int> arr(n + 1);
+    arr[1] = 0;
+    
+    for (int i = 2; i <= n; i++) {
+        arr[i] = arr[i - 1] + 1;
+
+        if (i % 2 == 0) {
+            arr[i] = min(arr[i], arr[i / 2] + 1);
+        }
+
+        if (i % 3 == 0) {
+            arr[i] = min(arr[i], arr[i / 3] + 1);
+        }
+    }
+
+    cout << arr[n];
+
+    return 0;
+}
