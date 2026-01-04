239b3aa75f0bffab88349abf2626cc8ca6e42c57
남진우 <njinu0479@naver.com>
2026-01-04 21:40:41 +0900

    [Bronze V] Title: 등록, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    cout << "237\n";
+    cout << "njinu0479";
+    
+    return 0;
+}
