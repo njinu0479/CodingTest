0eed19cbb53f8c330f0dac21c35bfd878682a8c0
남진우 <njinu0479@naver.com>
2026-01-11 11:46:18 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n, total = 0;
+    for(int i = 0; i < 5; i++) {
+        cin >> n;
+        total += n;
+    }
+    cout << total;
+    
+    return 0;
+}
