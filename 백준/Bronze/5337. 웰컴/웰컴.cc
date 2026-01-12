b356b405b375b0369717c0c754f460b7f1d3a3fc
남진우 <njinu0479@naver.com>
2026-01-12 23:57:25 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    ios::sync_with_stdio(0);
+
+    cin.tie(0);
+
+    cout << ".  .   ." << "\n";
+
+    cout << "|  | _ | _. _ ._ _  _" << "\n";
+
+    cout << "|/\\|(/.|(_.(_)[ | )(/.";
+
+    return 0;
+
+}
