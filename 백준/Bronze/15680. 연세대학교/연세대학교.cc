a523f9d8d21478a04f110786313ad53cb5d56faa
남진우 <njinu0479@naver.com>
2026-01-18 20:57:58 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    if (n == 0) {
+        cout << "YONSEI";
+    } else {
+        cout << "Leading the Way to the Future";
+    }
+
+    return 0;
+}
