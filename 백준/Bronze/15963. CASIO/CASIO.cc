d8fbd577b9fe86fd1689dbf9d999d1d0a3163aa4
남진우 <njinu0479@naver.com>
2026-01-27 23:43:05 +0900

    [Bronze V] Title: CASIO, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    long long n, m;
+    cin >> n >> m;
+
+    if (n == m) {
+        cout << 1;
+    } else {
+        cout << 0;
+    }
+
+    return 0;
+}
