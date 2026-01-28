b42c695bc818d9071636d4924ef84a00b4c41e0f
남진우 <njinu0479@naver.com>
2026-01-28 22:39:59 +0900

    [Bronze V] Title: Plane, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n1, k1, n2, k2;
+    cin >> n1 >> k1 >> n2 >> k2;
+    cout << (n1 * k1) + (n2 * k2);
+
+    return 0;
+}
