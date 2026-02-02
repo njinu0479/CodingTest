fc75918953ea92377031d4e3af45d75682aa8213
남진우 <njinu0479@naver.com>
2026-02-03 00:30:27 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cout << R"ROOK(  ___  ___  ___
+  | |__| |__| |
+  |           |
+   \_________/
+    \_______/
+     |     |
+     |     |
+     |     |
+     |     |
+     |_____|
+  __/       \__
+ /             \
+/_______________\)ROOK";
+
+  return 0;
+}
