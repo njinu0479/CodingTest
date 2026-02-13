b4b8a0edae6167ca69e5beebaec02a21c13071ea
남진우 <njinu0479@naver.com>
2026-02-14 00:48:32 +0900

    [Bronze V] Title: Who is in the middle?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6840. Who is in the middle？/Who is in the middle？.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int arr[3];
+    cin >> arr[0] >> arr[1] >> arr[2];
+    sort(arr, arr + 3);
+    cout << arr[1];
+
+    return 0;
+}
