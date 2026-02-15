03ac5c6fe99cd2836aaeb5a898a8f7c1915e455e
남진우 <njinu0479@naver.com>
2026-02-16 00:04:15 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string s;
+    cin >> s;
+
+    if (s == "NLCS") {
+        cout << "North London Collegiate School";
+    }
+    else if (s == "BHA") {
+        cout << "Branksome Hall Asia";
+    }
+    else if (s == "KIS") {
+        cout << "Korea International School";
+    }
+    else if (s == "SJA") {
+        cout << "St. Johnsbury Academy";
+    }
+
+    return 0;
+}
