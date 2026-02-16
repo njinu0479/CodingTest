7e98102dbf41030a1dc2d251cf3fba0514daf894
남진우 <njinu0479@naver.com>
2026-02-16 21:03:38 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    long long n;
+    cin >> n;
+    
+    while (n--) {
+        cout << "SciComLove" << "\n";
+    }
+
+    return 0;
+}
