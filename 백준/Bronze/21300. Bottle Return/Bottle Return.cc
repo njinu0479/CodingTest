6295a499aed68561e8bc25a8464a39f4da43d512
남진우 <njinu0479@naver.com>
2026-02-21 23:08:00 +0900

    [Bronze V] Title: Bottle Return, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21300. Bottle Return/Bottle Return.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int a, b, c, d, e, f;
+    cin >> a >> b >> c >> d >> e >> f;
+    cout << (a + b + c + d + e + f) * 5 << "\n";
+
+    return 0;
+}
