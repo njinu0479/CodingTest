0b3e87acfeec22e1c867de87def4c6557d2de9c2
남진우 <njinu0479@naver.com>
2026-02-25 22:05:21 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string s;
+    cin >> s;
+
+    if (s == "SONGDO") {
+        cout << "HIGHSCHOOL";
+    } else if (s == "CODE") {
+        cout << "MASTER";
+    } else if (s == "2023") {
+        cout << "0611";
+    } else if (s == "ALGORITHM") {
+        cout << "CONTEST";
+    }
+
+    return 0;
+}
