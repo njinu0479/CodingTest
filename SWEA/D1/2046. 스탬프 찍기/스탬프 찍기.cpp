a8dc3e937f3c5452422797114350828ecb307203
남진우 <njinu0479@naver.com>
2026-04-29 15:40:49 +0900

    [D1] Title: 스탬프 찍기, Time: 4 ms, Memory: 396 KB -BaekjoonHub

---
+++ SWEA/D1/2046. 스탬프 찍기/스탬프 찍기.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int i, n;
+	cin >> n;
+    
+    for(int i = 0; i < n; i++) {
+        cout << '#';
+    }
+    
+	return 0;
+}
