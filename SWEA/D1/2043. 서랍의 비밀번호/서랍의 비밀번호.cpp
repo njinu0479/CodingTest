3c27b4287d9f9652317c5e2dd6bb5895c3ac8aa9
남진우 <njinu0479@naver.com>
2026-05-14 14:02:01 +0900

    [D1] Title: 서랍의 비밀번호, Time: 4 ms, Memory: 396 KB -BaekjoonHub

---
+++ SWEA/D1/2043. 서랍의 비밀번호/서랍의 비밀번호.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int a, b;
+    cin >> a >> b;
+    
+    cout << abs(a - b) + 1;
+    
+	return 0;
+}
